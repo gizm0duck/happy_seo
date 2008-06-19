f3305352114ba74afd826b2f61527440b4a9fcba
shane wolf <shanewolf@gmail.com>
2008-06-18 19:53:58 -0700

    TONS of rework because it was attrocious before

--- spec/lib/happy_seo_spec.rb
+++ spec/lib/happy_seo_spec.rb
@@ -1,74 +1,112 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 class Event < ActiveRecord::Base
-  
+  happy_seo
 end
 
 describe HappySeo do
   before(:each) do
-    event = mock_model(Event, {:id => 1, 
-      :name         => 'Ohio State vs Wisconsin', 
+    ActiveRecord::Schema.define do
+      create_table :events, :force => true do |t|
+        t.column :name,         :string
+        t.column :city_name,    :string
+        t.column :tags,         :string
+        t.column :description,  :string
+      end
+    end
+    
+    @event = Event.create({:name => 'Ohio State vs Wisconsin', 
       :city_name    => 'Columbus', 
       :tags         => 'fun, football, horsehoe', 
-      :description  => 'Ohio State looks to extend their winning streak'})
-    @happy_seo = HappySeo.new(event)
+      :description  => 'Ohio State looks to extend their winning streak'})    
+  end
+  
+  after(:each) do
+    Event.delete_all
   end
   
   it "should generate an optimized seo_id in the form of id-name by default" do
-    @happy_seo.seo_id.should eql('1-ohio-state-vs-wisconsin')
+    @event.seo_id.should eql('1-ohio-state-vs-wisconsin')
   end
   
   it "should generate an optimized seo_id in the form of id-city_name-name when custom attributes are applied" do
-    @happy_seo.seo_id( [:id, :city_name, :name] ).should eql('1-columbus-ohio-state-vs-wisconsin')
+    @event.seo_id( [:id, :city_name, :name] ).should eql('1-columbus-ohio-state-vs-wisconsin')
   end
   
   it "should generate keywords string based on name, description, tags by default" do
-    @happy_seo.meta_keywords.should eql('Ohio State vs Wisconsin, Ohio State looks to extend their winning streak, fun, football, horsehoe')
+    @event.meta_keywords.should eql('Ohio State vs Wisconsin, Ohio State looks to extend their winning streak, fun, football, horsehoe')
   end
   
   it "should generate keywords string based on name, city_name, tags when custom attributes are applied" do
-    @happy_seo.meta_keywords( [:name, :city_name, :tags] ).should eql('Ohio State vs Wisconsin, Columbus, fun, football, horsehoe')
+    @event.meta_keywords( [:name, :city_name, :tags] ).should eql('Ohio State vs Wisconsin, Columbus, fun, football, horsehoe')
   end
   
   it "should generate description string based on name, description, tags by default" do
-    @happy_seo.meta_description.should eql('Ohio State vs Wisconsin. Ohio State looks to extend their winning streak. fun, football, horsehoe')
+    @event.meta_description.should eql('Ohio State vs Wisconsin. Ohio State looks to extend their winning streak. fun, football, horsehoe')
   end
   
   it "should generate description string based on name, description, city_name by default" do
-    @happy_seo.meta_description( [:name, :description, :city_name] ).should eql('Ohio State vs Wisconsin. Ohio State looks to extend their winning streak. Columbus')
+    @event.meta_description( [:name, :description, :city_name] ).should eql('Ohio State vs Wisconsin. Ohio State looks to extend their winning streak. Columbus')
   end
   
   it "should be able to update the default seo_id attributes" do
-    HappySeo.default_seo_id_attributes = [ :name ]
-    @happy_seo.seo_id.should eql('ohio-state-vs-wisconsin')
+    class << Event
+      def default_seo_id_attributes
+        [ :name ] 
+      end
+    end
+    @event.seo_id.should eql('ohio-state-vs-wisconsin')
   end
   
   it "should be able to update the default meta_keyword attributes" do
-    HappySeo.default_meta_keywords_attributes = [ :name ]
-    @happy_seo.meta_keywords.should eql('Ohio State vs Wisconsin')
+    class << Event
+      def default_meta_keywords_attributes
+        [ :name ] 
+      end
+    end
+    @event.meta_keywords.should eql('Ohio State vs Wisconsin')
   end
   
   it "should be able to update the default meta_description attributes" do
-    HappySeo.default_meta_description_attributes = [ :name ]
-    @happy_seo.meta_description.should eql('Ohio State vs Wisconsin')
+    class << Event
+      def default_meta_description_attributes
+        [ :name ] 
+      end
+    end
+    @event.meta_description.should eql('Ohio State vs Wisconsin')
   end
 end
 
 describe "clean_string" do
   before(:each) do
-    event = mock_model(Event, {:id => 1, :name => 'Ohio State vs Wisconsin'})
-    @happy_seo = HappySeo.new(event)
+    ActiveRecord::Schema.define do
+      create_table :events, :force => true do |t|
+        t.column :name,         :string
+        t.column :city_name,    :string
+        t.column :tags,         :string
+        t.column :description,  :string
+      end
+    end
+    
+    @event = Event.create({:name => 'Ohio State vs Wisconsin', 
+      :city_name    => 'Columbus', 
+      :tags         => 'fun, football, horsehoe', 
+      :description  => 'Ohio State looks to extend their winning streak'})
+  end
+  
+  after(:each) do
+    Event.delete_all
   end
   
   it "should downcase the input" do
-    @happy_seo.send(:clean_string, 'hAPpy').should eql('happy')
+    @event.send(:clean_string, 'hAPpy').should eql('happy')
   end
   
   it "should remove illegal characters" do
-    @happy_seo.send(:clean_string, "h:[a\"p'p;y").should eql('happy')
+    @event.send(:clean_string, "h:[a\"p'p;y").should eql('happy')
   end
   
   it "should replace spaces with dashes" do
-    @happy_seo.send(:clean_string, "happy seo").should eql('happy-seo')
+    @event.send(:clean_string, "happy seo").should eql('happy-seo')
   end
 end
